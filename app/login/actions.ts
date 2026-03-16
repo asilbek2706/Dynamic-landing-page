4d680ff15a2c58ba1653a700d85311e43608d3b5
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-03-16 21:48:44 +0500

    feat: add login action and server-only import for Supabase integration

---
+++ app/login/actions.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+
+export async function login(formData: FormData) {
+    const supabase = await createClient();
+
+    const data = {
+        email: formData.get('email') as string,
+        password: formData.get('password') as string,
+    }
+
+    const { error } = await supabase.auth.signInWithPassword(data);
+
+    if (error) {
+       return { success: false as const, error: error.message };
+    }
+
+    return { success: true as const };
+}
